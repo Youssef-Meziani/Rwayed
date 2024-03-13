abd5f69201f0d7bb02507940afbf8e4661ab8e32
omarbennani190 <153104871+omarbennani190@users.noreply.github.com>
2024-03-13 23:08:16 +0000

    Shop personnalisation
    Ce commit implémente plusieurs améliorations majeures sur la page Shop :
    
    1. Affichage des pneus avec leurs détails:
    
        Ajout d'une boucle pour afficher chaque pneu avec ses informations (nom, description, prix, etc.).
        Amélioration de la mise en forme pour une meilleure lisibilité.
    
    2. Quickview avec AJAX:
    
        Intégration d'une fonctionnalité Quickview accessible via un bouton sur chaque pneu.
        Utilisation d'AJAX pour charger dynamiquement les détails du pneu dans une modale.
    
    3. Design Pattern Strategy pour la conversion DTO-Entity:
    
        Implémentation du design pattern Strategy pour transformer les objets DTO (Data Transfer Object) reçus de l'API en objets Entity utilisés par l'application.
        Amélioration de la flexibilité et de la maintenabilité du code en séparant la logique de conversion de la structure des données.

--- Rwayed/src/Entity/Photo.php
+++ Rwayed/src/Entity/Photo.php
@@ -30,6 +30,11 @@ class Photo
     {
         return $this->id;
     }
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+        return $this;
+    }
 
     public function getImageFile(): ?File
     {
