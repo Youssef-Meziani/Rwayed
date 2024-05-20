0291b15c62bb83693cfafe341704cabd8e4e6b33
HASHAS Jad el mawla <jouj969@gmail.com>
2024-05-20 21:42:43 +0100

    Ajout de la fonctionnalité de suivi des commandes
    - Implémentation de codes uniques pour chaque commande, permettant un suivi efficace des commandes.
    - Intégration d'une interface utilisateur pour consulter le statut de la commande

--- Rwayed/src/Controller/TrackOrderController.php
+++ Rwayed/src/Controller/TrackOrderController.php
@@ -2,15 +2,49 @@
 
 namespace App\Controller;
 
+use App\Form\TrackOrderType;
+use App\Repository\CommandeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class TrackOrderController extends AbstractController
 {
     #[Route('/track-order', name: 'track-order')]
-    public function index(): Response
+    public function index(Request $request, CommandeRepository $commandeRepository): Response
     {
-        return $this->render('track-order.twig');
+        if (!$this->getUser()) {
+            $session = $request->getSession();
+            $session->set('referer_checkout', $request->getUri());
+            return $this->redirectToRoute('app_login');
+        }
+        $form = $this->createForm(TrackOrderType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            if ($request->isXmlHttpRequest()) {
+                $codeUnique = $data['order_id'];
+                $user = $this->getUser();
+
+                $commande = $commandeRepository->findOneBy([
+                    'codeUnique' => $codeUnique,
+                    'adherent' => $user,
+                ]);
+                if (!$commande) {
+                    return new JsonResponse(['status' => 'error', 'message' => 'Order not found'], Response::HTTP_NOT_FOUND);
+                }
+
+                return new JsonResponse(['status' => 'success', 'orderStatus' => $commande->getStatutsCommandeLabel()]);
+            }
+        }
+
+        return $this->render('track-order.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
